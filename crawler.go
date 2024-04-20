55b7934fcc3849902e82278d72d3cba01c0bc551
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-20 21:16:40 +0700

    testing visit

--- crawler.go
+++ crawler.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
 
-func WebCrawler() {
-
+func crawler() {
+	found := false
+	queue := make(map[string]bool)
 	c := colly.NewCollector(colly.AllowedDomains(
-		"id.wikipedia.org"))
+		"en.wikipedia.org"))
 
 	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		fmt.Println(h.Text)
+		link := h.Attr("href")
+		if strings.HasPrefix(link, "/wiki/") &&
+			!strings.Contains(link, "File:") &&
+			!strings.Contains(link, "Help:") &&
+			!strings.Contains(link, "Category:") &&
+			!strings.Contains(link, "Wikipedia:") &&
+			!strings.Contains(link, "Talk:") &&
+			!strings.Contains(link, "Special:") &&
+			!strings.Contains(link, "Portal:") &&
+			!strings.Contains(link, "Template:") &&
+			("/wiki/Main_Page" != link) &&
+			!queue["https://en.wikipedia.org"+link] {
+			queue["https://en.wikipedia.org"+link] = true
+		}
+		if link == "/wiki/Novel_Baswaaaedan" {
+			fmt.Println("KETEMU")
+			found = true
+		}
 	})
 
-	c.Visit("https://id.wikipedia.org/wiki/Halaman_Utama")
+	queue["https://en.wikipedia.org/wiki/Gibran_Rakabuming"] = true
+	c.Visit("https://en.wikipedia.org/wiki/Gibran_Rakabuming")
+
+}
+
+func main() {
+	crawler()
 }
