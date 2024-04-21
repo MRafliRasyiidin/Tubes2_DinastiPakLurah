7eb8ba25c5102b6d90b4b652f81a598dd6df02a6
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-22 00:20:44 +0700

    feat: Single solution BFS

--- main.go
+++ main.go
@@ -34,7 +34,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	"sync/atomic"
 	"time"
 
 	"github.com/gocolly/colly/v2"
@@ -43,47 +42,19 @@ import (
 
 func crawler(start string, target string) {
 	queue := safeorderedmap.New[bool]()
-	var currPath []string
-	var continueSearch int32 = 1
+	path := safeorderedmap.New[string]()
+	done := make(chan bool)
 	c := colly.NewCollector(
 		colly.AllowedDomains("en.wikipedia.org"),
 		colly.Async(true),
 	)
-	done := make(chan bool)
+	c.AllowURLRevisit = false
 
-	// c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 20})
-
-	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		link := h.Attr("href")
-		if atomic.LoadInt32(&continueSearch) == 1 {
-			_, exists := queue.Get(h.Request.AbsoluteURL(link))
-			if strings.HasPrefix(link, "/wiki/") &&
-				!strings.Contains(link, "File:") &&
-				!strings.Contains(link, "Help:") &&
-				!strings.Contains(link, "Category:") &&
-				!strings.Contains(link, "Wikipedia:") &&
-				!strings.Contains(link, "Talk:") &&
-				!strings.Contains(link, "Special:") &&
-				!strings.Contains(link, "Portal:") &&
-				!strings.Contains(link, "Template:") &&
-				!strings.Contains(link, "MediaWiki:") &&
-				!strings.Contains(link, "User:") &&
-				!strings.Contains(link, "_talk:") &&
-				(link != "/wiki/Main_Page") &&
-				!exists {
-				queue.Add(h.Request.AbsoluteURL(link), true)
-				fmt.Println(h.Request.AbsoluteURL(link), atomic.LoadInt32(&continueSearch))
-			}
-		}
-		if link == "/wiki/"+target {
-			atomic.StoreInt32(&continueSearch, 0)
-			done <- true
-			return
-		}
-	})
+	// Wtf is even Parallelism: 1000?? Me brainrot big number equals good
+	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 1000, Delay: 20 * time.Millisecond})
 
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
+		// fmt.Println("Visiting", r.URL)
 	})
 
 	c.OnError(func(_ *colly.Response, err error) {
@@ -91,34 +62,68 @@ func crawler(start string, target string) {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
+		// I need to track how many links are fully visited
+		fmt.Println("Visited", r.Request.URL)
+	})
+
+	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+		link := h.Attr("href")
+		visited, _ := c.HasVisited(link)
+		_, exists := queue.Get(h.Request.AbsoluteURL(link))
+		if strings.HasPrefix(link, "/wiki/") &&
+			!strings.Contains(link, "File:") &&
+			!strings.Contains(link, "Help:") &&
+			!strings.Contains(link, "Category:") &&
+			!strings.Contains(link, "Wikipedia:") &&
+			!strings.Contains(link, "Talk:") &&
+			!strings.Contains(link, "Special:") &&
+			!strings.Contains(link, "Portal:") &&
+			!strings.Contains(link, "Template:") &&
+			!strings.Contains(link, "MediaWiki:") &&
+			!strings.Contains(link, "User:") &&
+			!strings.Contains(link, "_talk:") &&
+			link != "/wiki/Main_Page" &&
+			!exists && !visited {
+			queue.Add(h.Request.AbsoluteURL(link), true)
+			path.Add(h.Request.AbsoluteURL(link), h.Request.URL.String())
+			// fmt.Println(h.Request.AbsoluteURL(link))
+		}
+		if link == "/wiki/"+target {
+			path.Add(h.Request.AbsoluteURL(link), h.Request.URL.String())
+			done <- true
+			return
+		}
 	})
 
 	c.OnScraped(func(r *colly.Response) {
-		if atomic.LoadInt32(&continueSearch) == 0 {
-			return
-		}
-		fmt.Println("Finished", r.Request.URL)
+		// fmt.Println("Finished", r.Request.URL)
 		queue.Delete(r.Request.URL.String())
 		var link string
 		link = ""
 		queue.Each(func(k string, e bool) {
 			link = k
-			currPath = append(currPath, link)
-			if atomic.LoadInt32(&continueSearch) == 1 {
-				c.Visit(link)
-			}
+			c.Visit(link)
 		})
 	})
 
-	currPath = append(currPath, "https://en.wikipedia.org/wiki/"+start)
 	c.Visit("https://en.wikipedia.org/wiki/" + start)
 	go c.Wait()
 	<-done
-	// fmt.Println(currPath)
+
+	key := "https://en.wikipedia.org/wiki/" + target
+	for key != "https://en.wikipedia.org/wiki/"+start {
+		fmt.Println("Key : ", key)
+		value, _ := path.Get(key)
+		key = value
+	}
+	fmt.Println("Key : ", key)
+	// path.Each(func(key, value string) {
+	// 	fmt.Println("PATH:", key, value)
+	// })
 }
 
 func main() {
 	start := time.Now()
-	crawler("Ring_of_Fire", "United_States")
+	crawler("MiHoYo", "Intel_8080")
 	fmt.Println(time.Since(start))
 }
