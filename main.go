e3217d484d744e548d7a5e79b5d0ed80f75e8787
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-21 12:12:53 +0700

    anu stopping pake chan

--- main.go
+++ main.go
@@ -34,39 +34,51 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync/atomic"
+	"time"
 
 	"github.com/gocolly/colly/v2"
-	orderedmap "github.com/wk8/go-ordered-map"
+	"github.com/thalesfsp/go-common-types/safeorderedmap"
 )
 
 func crawler(start string, target string) {
-	queue := orderedmap.New()
+	queue := safeorderedmap.New[bool]()
 	var currPath []string
-	continueSearch := true
+	var continueSearch int32 = 1
 	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"))
+		colly.AllowedDomains("en.wikipedia.org"),
+		colly.Async(true),
+	)
+	done := make(chan bool)
+
+	// c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 20})
+
 	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
 		link := h.Attr("href")
-		if link == "/wiki/"+target {
-			continueSearch = false
+		if atomic.LoadInt32(&continueSearch) == 1 {
+			_, exists := queue.Get(h.Request.AbsoluteURL(link))
+			if strings.HasPrefix(link, "/wiki/") &&
+				!strings.Contains(link, "File:") &&
+				!strings.Contains(link, "Help:") &&
+				!strings.Contains(link, "Category:") &&
+				!strings.Contains(link, "Wikipedia:") &&
+				!strings.Contains(link, "Talk:") &&
+				!strings.Contains(link, "Special:") &&
+				!strings.Contains(link, "Portal:") &&
+				!strings.Contains(link, "Template:") &&
+				!strings.Contains(link, "MediaWiki:") &&
+				!strings.Contains(link, "User:") &&
+				!strings.Contains(link, "_talk:") &&
+				(link != "/wiki/Main_Page") &&
+				!exists {
+				queue.Add(h.Request.AbsoluteURL(link), true)
+				fmt.Println(h.Request.AbsoluteURL(link), atomic.LoadInt32(&continueSearch))
+			}
 		}
-		_, exists := queue.Get(h.Request.AbsoluteURL(link))
-		if strings.HasPrefix(link, "/wiki/") &&
-			!strings.Contains(link, "File:") &&
-			!strings.Contains(link, "Help:") &&
-			!strings.Contains(link, "Category:") &&
-			!strings.Contains(link, "Wikipedia:") &&
-			!strings.Contains(link, "Talk:") &&
-			!strings.Contains(link, "Special:") &&
-			!strings.Contains(link, "Portal:") &&
-			!strings.Contains(link, "Template:") &&
-			!strings.Contains(link, "MediaWiki:") &&
-			!strings.Contains(link, "User:") &&
-			!strings.Contains(link, "_talk:") &&
-			(link != "/wiki/Main_Page") &&
-			!exists {
-			queue.Set(h.Request.AbsoluteURL(link), true)
-			fmt.Println(h.Request.AbsoluteURL(link))
+		if link == "/wiki/"+target {
+			atomic.StoreInt32(&continueSearch, 0)
+			done <- true
+			return
 		}
 	})
 
@@ -79,31 +91,34 @@ func crawler(start string, target string) {
 	})
 
 	c.OnResponse(func(r *colly.Response) {
-		// fmt.Println("Visited", r.Request.URL)
 	})
 
 	c.OnScraped(func(r *colly.Response) {
+		if atomic.LoadInt32(&continueSearch) == 0 {
+			return
+		}
 		fmt.Println("Finished", r.Request.URL)
 		queue.Delete(r.Request.URL.String())
 		var link string
 		link = ""
-		for key := queue.Oldest(); key != nil; key = key.Next() {
-			link = key.Key.(string)
-			break
-		}
-		currPath = currPath[:len(currPath)-1]
-		if continueSearch {
+		queue.Each(func(k string, e bool) {
+			link = k
 			currPath = append(currPath, link)
-			c.Visit(link)
-		}
+			if atomic.LoadInt32(&continueSearch) == 1 {
+				c.Visit(link)
+			}
+		})
 	})
 
 	currPath = append(currPath, "https://en.wikipedia.org/wiki/"+start)
 	c.Visit("https://en.wikipedia.org/wiki/" + start)
-	c.Wait()
-	fmt.Println(currPath)
+	go c.Wait()
+	<-done
+	// fmt.Println(currPath)
 }
 
 func main() {
-	crawler("Jokowi", "Indonesia")
+	start := time.Now()
+	crawler("Ring_of_Fire", "United_States")
+	fmt.Println(time.Since(start))
 }
