15426c6b6c82e4f879caedeef7630c7c0a65322e
InTheRain <indrswara@gmail.com>
2024-04-22 22:52:38 +0700

    ids with async, dont know still feel slow

--- dfs.go
+++ dfs.go
@@ -3,81 +3,10 @@ package main
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/gocolly/colly/v2"
 )
-
-func crawlerOLD(start string, target string) {
-	maxDepth := 1
-	for {
-		if dfsOld(start, target, maxDepth, 1, []string{}) {
-			break
-		}
-		maxDepth++
-	}
-}
-
-var continueSearch bool = true
-
-func dfsOld(start string, target string, maxDepth, depth int, currPath []string) bool {
-	if depth > maxDepth || !continueSearch {
-		return false
-	}
-
-	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org"))
-
-	c.OnHTML("a[href]", func(h *colly.HTMLElement) {
-		link := h.Attr("href")
-		if continueSearch && !isInPath(link, currPath) {
-			if link == "/wiki/"+strings.ReplaceAll(target, " ", "_") {
-				continueSearch = false
-				fmt.Println("Found in depth:", depth)
-			}
-			if len(currPath) < maxDepth && !strings.HasPrefix(link, "#") && !strings.HasPrefix(link, "http") {
-				if strings.HasPrefix(link, "/wiki/") &&
-					!strings.Contains(link, "File:") &&
-					!strings.Contains(link, "Help:") &&
-					!strings.Contains(link, "Category:") &&
-					!strings.Contains(link, "Wikipedia:") &&
-					!strings.Contains(link, "Talk:") &&
-					!strings.Contains(link, "Special:") &&
-					!strings.Contains(link, "Portal:") &&
-					!strings.Contains(link, "Template:") &&
-					!strings.Contains(link, "MediaWiki:") &&
-					!strings.Contains(link, "User:") &&
-					!strings.Contains(link, "_talk:") &&
-					(link != "/wiki/Main_Page") {
-					fmt.Printf("%s - depth: %d\n", h.Request.AbsoluteURL(link), depth)
-					newPath := append([]string(nil), currPath...)
-					dfsOld(extractTitle(link), target, maxDepth, depth+1, append(newPath, link))
-				}
-			}
-		}
-	})
-
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	c.OnError(func(_ *colly.Response, err error) {
-		fmt.Println("Something went wrong:", err)
-	})
-
-	c.OnResponse(func(r *colly.Response) {
-		// fmt.Println("Visited", r.Request.URL)
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
-
-	if continueSearch {
-		c.Visit("https://en.wikipedia.org/wiki/" + strings.ReplaceAll(start, " ", "_"))
-		c.Wait()
-	}
-	return !continueSearch
-}
 
 func crawler(start, target string) ([]string, bool) {
 	maxDepth := 3
@@ -87,6 +16,11 @@ func crawler(start, target string) ([]string, bool) {
 	}
 	return nil, false
 }
+
+var (
+	isFound bool
+	mu      sync.Mutex
+)
 
 func ids(start, target string, maxDepth int) ([]string, bool) {
 	found := false
@@ -106,13 +40,14 @@ func ids(start, target string, maxDepth int) ([]string, bool) {
 	return nil, false
 }
 
-var isFound bool = false
-
 func dfs(start, target string, depth, maxDepth int, visited map[string]bool, path []string) ([]string, bool) {
 	if depth > maxDepth {
 		return nil, false
 	}
 	if extractTitle(start) == extractTitle(target) {
+		mu.Lock()
+		isFound = true
+		mu.Unlock()
 		return path, true
 	}
 	if visited[start] {
@@ -127,14 +62,12 @@ func dfs(start, target string, depth, maxDepth int, visited map[string]bool, pat
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 		if !isFound {
-
-			/*
-				Bagian ini digunakan untuk single solution, atau multiple solution
-			*/
 			if link == "/wiki/"+strings.ReplaceAll(target, " ", "_") {
+				mu.Lock()
 				isFound = true
 				ans := append(path, extractTitle(link))
 				printPath(ans)
+				mu.Unlock()
 			}
 			newLink := extractTitle(link)
 			if !strings.HasPrefix(link, "#") && !strings.HasPrefix(link, "http") {
@@ -152,15 +85,7 @@ func dfs(start, target string, depth, maxDepth int, visited map[string]bool, pat
 					!strings.Contains(link, "_talk:") &&
 					(link != "/wiki/Main_Page") &&
 					!isInPath(newLink, path) {
-					var newPath []string
-					result, found := dfs(newLink, target, depth+1, maxDepth, visited, newPath)
-					/*
-						bagian ini berhubungan dengan single solution dan multiple solution
-					*/
-					if found {
-						isFound = true
-						printPath(result)
-					}
+					dfs(newLink, target, depth+1, maxDepth, visited, append(path, newLink))
 				}
 			}
 		}
