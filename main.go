cf87a6d890700e867e96c52a7df11debac49c6b2
hannoobz <13522100@std.stei.itb.ac.id>
2024-04-25 09:09:26 +0700

    feat: path maker

--- main.go
+++ main.go
@@ -33,39 +33,45 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"sync"
 	"time"
 
 	"github.com/thalesfsp/go-common-types/safeorderedmap"
 )
 
-func dfs(node, ender string, visited map[string]bool, adjacencyList *safeorderedmap.SafeOrderedMap[[]string], path []string, paths *[][]string, depth int32) {
+func dfsPathMaker(node, ender string, adjacencyList *safeorderedmap.SafeOrderedMap[[]string], path []string, paths *[][]string, depth int32) {
 	adjacent, _ := adjacencyList.Get(node)
-	// fmt.Println(path)
 	finish := node == "https://en.wikipedia.org/wiki/"+ender
+
 	path = append(path, node)
-	if finish {
-		*paths = append(*paths, path)
+	pathCopy := []string{}
+	for _, value := range path {
+		pathCopy = append(pathCopy, value)
+	}
+	isExist := false
+	for _, value := range *paths {
+		if reflect.DeepEqual(value, pathCopy) {
+			isExist = true
+			break
+		}
+	}
+	if finish && !isExist {
+		*paths = append(*paths, pathCopy)
 	} else {
 		if len(path) < int(depth)+1 {
 			for _, neighbor := range adjacent {
-				if !visited[neighbor] {
-					dfs(neighbor, ender, visited, adjacencyList, path, paths, depth)
-				}
+				dfsPathMaker(neighbor, ender, adjacencyList, pathCopy, paths, depth)
 			}
 		}
 	}
-	path = path[:len(path)-1]
-	visited[node] = false
 }
 
 func converter(adjacencyList *safeorderedmap.SafeOrderedMap[[]string], start, end string, depth int32) [][]string {
-	visited := make(map[string]bool)
+	depth++
 	var path []string
 	var paths [][]string
-
-	dfs("https://en.wikipedia.org/wiki/"+start, end, visited, adjacencyList, path, &paths, depth)
-
+	dfsPathMaker("https://en.wikipedia.org/wiki/"+start, end, adjacencyList, path, &paths, depth)
 	return paths
 }
 
@@ -75,71 +81,20 @@ func main() {
 	start := time.Now()
 	path := safeorderedmap.New[[]string]()
 	linkStart := "Medan_Prijaji"
-	linkTarget := "Adolf_Hitler"
+	linkTarget := "Hitler"
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		crawlerBFS(linkStart, linkTarget, path, &depth)
+		// crawlerIDS(linkStart, linkTarget, path, &depth)
 	}()
 	wg.Wait()
-	// path.Each(func(key string, value []string) {
-	// 	fmt.Println(key)
-	// 	fmt.Println(value)
-	// })
-	fmt.Println("DEPTH IS", depth)
+	fmt.Println("DEPTH IS", depth+1)
 	paths := converter(path, linkTarget, linkStart, depth)
-	// // fmt.Println(paths)
-	min := 1000
 	for _, res := range paths {
-		fmt.Println("AAA")
-		if min > len(res) && res[len(res)-1] == "https://en.wikipedia.org/wiki/"+linkStart {
-			fmt.Println("ABA")
-			min = len(res)
+		if res[len(res)-1] == "https://en.wikipedia.org/wiki/"+linkStart {
+			fmt.Println(res)
 		}
 	}
-	for _, res := range paths {
-
-		if len(res) > 0 {
-
-			if res[len(res)-1] == "https://en.wikipedia.org/wiki/"+linkStart && len(res) <= min {
-
-				fmt.Println(res)
-			}
-		}
-	}
-	fmt.Println("IT IS DONE", time.Since(start), paths[0])
-	// path.Each(func(key string, value []string) {
-	// 	fmt.Println("Key", key)
-	// 	fmt.Println(value)
-	// })
-	// path.Each(func(key string, value []string) {
-	// 	fmt.Println("Key", key)
-	// 	fmt.Println("Value,", value)
-	// })
-
+	fmt.Println("Runtime:", time.Since(start), paths[0])
 }
-
-// path.Each(func(key string, value []string) {
-// 	fmt.Println(value)
-// })
-// }
-
-// key := "https://en.wikipedia.org/wiki/" + target
-// expPath := []string{}
-// for key != "https://en.wikipedia.org/wiki/"+start {
-// 	fmt.Println("Key : ", key)
-// 	expPath = append(expPath, key)
-// 	value, _ := path.Get(key)
-// 	key = value.(string)
-// }
-
-// for i, j := 0, len(expPath)-1; i < j; i, j = i+1, j-1 {
-// 	expPath[i], expPath[j] = expPath[j], expPath[i]
-// }
-
-// jsonStr, err := json.Marshal(expPath)
-// if err != nil {
-// 	fmt.Printf("Error: %s", err.Error())
-// } else {
-// 	fmt.Println(string(jsonStr))
-// }
