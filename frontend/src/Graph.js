03e1ae93c95ccad8f3741480b716f2940d148c2d
InTheRain <indrswara@gmail.com>
2024-04-25 22:29:00 +0700

    add graph multiple path

--- frontend/src/Graph.js
+++ frontend/src/Graph.js
@@ -11,37 +11,54 @@ function NodeGraph({ darkmode, start, target }) {
       generateGraph(nodes);
     };
 
-    const fetchNodeList = async (start, target) => {
+    const fetchNodeList = async (start, target, listSolution) => {
       // template
-      const nodeList = ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5'];
+      const nodeLists = [
+        ['Node 1', 'Node 2', 'Node 3', 'Node 4', 'Node 5'],
+        ['node1', 'node2', 'node3'], 
+        ['waduh', 'kuda', 'anjing'],
+      ];
+      for(let node of nodeLists){
+        node.push(target);
+        node.unshift(start);
+      }
 
-      nodeList.push(target);
-      nodeList.unshift(start);
-
-      return nodeList;
+      console.log(nodeLists);
+      return nodeLists;
     };
 
-    const generateGraph = (nodes) => {
+    const generateGraph = (paths) => {
       var graphData = {
         nodes: [],
         edges: []
       };
 
-      for (let i = 0; i < nodes.length; i++) {
-        graphData.nodes.push({
-          id: `${uuidv4()}`,
-          label: nodes[i],
-          title: nodes[i],
-        });
+      for(let path of paths){
+        for (let i = 0; i < path.length; i++) {
+          const node = path[i]
+          const isNewNode = !graphData.nodes.find(n => n.label === node);
 
-        if(i >= 1){
-            if (i < nodes.length) {
-              graphData.edges.push({
-                from: graphData.nodes[i-1].id,
-                to: graphData.nodes[i].id,
-                id: `${uuidv4()}`,
-              });
-            }
+          if(isNewNode){
+            graphData.nodes.push({
+              id: `${uuidv4()}`,
+              label: path[i],
+              title: path[i],
+            });
+          }
+  
+          if(i > 0){
+                const fromNode = path[i - 1];
+                const toNode = path[i];
+                const fromNodeObj = graphData.nodes.find(n => n.label === fromNode);
+                const toNodeObj = graphData.nodes.find(n => n.label === toNode);
+                if(fromNodeObj && toNodeObj){
+                  graphData.edges.push({
+                    from: fromNodeObj.id,
+                    to: toNodeObj.id,
+                    id: `${uuidv4()}`,
+                  });
+              }
+          }
         }
       }
       setGraph(graphData);
@@ -65,9 +82,18 @@ function NodeGraph({ darkmode, start, target }) {
       dashes: false,
     },
     height: '700px',
-    layout: {
-      hierarchical: false
+    smooth: {
+      type: 'continuous' // Set the type of edge smoothing
     },
+    layout: {
+      hierarchical: false,
+      randomSeed: 1, 
+      improvedLayout: true, 
+      avoidOverlap: true, 
+    },
+    physics: true, 
+    // distance: 150,
+
   };
 
   const events = {
@@ -80,7 +106,7 @@ function NodeGraph({ darkmode, start, target }) {
 
   return (
     <div>
-      <div style={{ width: '175vh', height: '100vh' }} className="flex items-center align-middle justify-center border  bg-slate-300 rounded-xl  mt-40 mb-10">
+      <div style={{ width: '100vh', height: '75vh' }} className="flex items-center align-middle justify-center border  bg-slate-300 rounded-xl  mt-40 mb-10">
         <Graph
           key={uuidv4()}
           options={options}
